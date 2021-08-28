929e360b5f7be85756af628687a1aa6f28157156
Aleksa <acojanko@gmail.com>
2021-08-28 21:29:36 +0200

    Add project files.

---
+++ DiscordBOT/Modules/commands.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace PilliesBOT.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+
+
+        [Command("Who")]
+        public async Task Question()
+        {
+            await ReplyAsync("Asked!");
+        }
+
+        [Command("echo")]
+        public async Task Say([Remainder] string text)
+        {
+            await ReplyAsync(text);
+        }
+
+
+
+        [RequireContext(ContextType.Guild)]
+        public class Set : ModuleBase
+        {
+            [Command("mention")]
+            public async Task Mention(SocketGuildUser user)
+            {
+                await ReplyAsync($"Hello {user.Mention}");
+            }
+        }
+        [Command("kck")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        public async Task Kick([Remainder] SocketGuildUser user)
+        {
+
+            await ReplyAsync($"Wow this {user.Mention} actually managed to get kicked :clap: :clap:");
+            await user.KickAsync();
+        }
+    
+    [Command("swing")]
+      [RequireUserPermission(GuildPermission.BanMembers)]
+    public async Task Ban([Remainder] SocketGuildUser user)
+    {
+          
+            await ReplyAsync($"THIS PERSON -> {user.Mention} GOT BANNED :dizzy_face: :dizzy_face: ");
+        await user.BanAsync();
+    }
+
+
+
+}
+}
---
+++ DiscordBOT/main.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace PilliesBOT
+{
+    class main
+
+    {   
+        //You can only await something that returns Task
+        //Creating a instance of main class and awaiting RunBotAsync()
+        static async Task Main() => await new main().RunBotAsync();
+
+        //Creating my necessery fields
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+         
+        public async Task RunBotAsync()
+        {
+            
+            //Assign my fields 
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            //If someone asks for either of these types then provide these instaces
+            _services = new ServiceCollection()
+                //Use the object that is provided if someone asks for it
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            //storing the token 
+
+           
+
+
+            _client.Log += _client_Log;
+
+            //Executing 
+
+            await RegisterCommandsAsync();
+            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"));
+            await _client.StartAsync();
+            await Task.Delay(-1);
+           
+        }
+    
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        // If bot receives a message fire HandleCommandAsync
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        //Check conditions before executing commands! 
+        public async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+           
+            // argPos integer that holds and is being check if the prefix is present! 
+
+            int argPos = 0;
+            if (message.HasStringPrefix("!", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+          
+        }
+    }
+}
